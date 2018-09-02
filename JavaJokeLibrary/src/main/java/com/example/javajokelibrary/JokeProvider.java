f732e3f1028a10b0861f6a4112cb3b0f520d4e2e
Kundan <kundank191@gmail.com>
2018-09-03 00:51:31 +0530

    feat : add jokes in library

--- JavaJokeLibrary/src/main/java/com/example/javajokelibrary/JokeProvider.java
+++ JavaJokeLibrary/src/main/java/com/example/javajokelibrary/JokeProvider.java
@@ -11,7 +11,15 @@ public class JokeProvider {
      */
     public List<Joke> getJokes(){
         List<Joke> jokes = new LinkedList<>();
-        jokes.add(new Joke("",""));
+        jokes.add(new Joke("Why is peter pan always flying?","Because he neverlands"));
+        jokes.add(new Joke("What happens to a frog's car when it breaks down?","It gets toad away"));
+        jokes.add(new Joke("What did the duck say when he bought lipstick?","Put it on my bill"));
+        jokes.add(new Joke("When a dad drives past a graveyard: Did you know that's a popular cemetery?","Yep, people are just dying to get in there"));
+        jokes.add(new Joke("What do you call a belt made out of watches?","A waist of time."));
+        jokes.add(new Joke("To understand what recursion is...","You must first understand what recursion is"));
+        jokes.add(new Joke("A ham sandwhich walks into a bar and orders a beer. The bartender says...","I'm sorry, we don't serve food here"));
+        jokes.add(new Joke("What's the best time to go to the dentist?","Tooth hurty."));
+        jokes.add(new Joke("Why can't bicycles stand on their own?","hey are two tired"));
         return jokes;
     }
 }
