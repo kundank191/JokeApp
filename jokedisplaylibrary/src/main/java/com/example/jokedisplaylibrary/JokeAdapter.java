c284465267d022ff6584d045205888500997caec
Kundan <kundank191@gmail.com>
2018-09-03 05:45:40 +0530

    feat : improve app ui

--- jokedisplaylibrary/src/main/java/com/example/jokedisplaylibrary/JokeAdapter.java
+++ jokedisplaylibrary/src/main/java/com/example/jokedisplaylibrary/JokeAdapter.java
@@ -1,7 +1,10 @@
 package com.example.jokedisplaylibrary;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +18,19 @@ import java.util.ArrayList;
  */
 public class JokeAdapter extends RecyclerView.Adapter<JokeAdapter.ViewHolder>{
 
+    // Using material 700 colors
+    private static String TYPE_COLOR = "700";
+
     class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView questionTV;
         TextView answerTV;
+        ConstraintLayout parentCL;
         ViewHolder(View itemView) {
             super(itemView);
             questionTV = itemView.findViewById(R.id.question_tv);
             answerTV = itemView.findViewById(R.id.answer_tv);
+            parentCL = itemView.findViewById(R.id.row_item);
         }
     }
 
@@ -48,10 +56,31 @@ public class JokeAdapter extends RecyclerView.Adapter<JokeAdapter.ViewHolder>{
         String[] jokeParts = joke.split("\n");
         holder.questionTV.setText(jokeParts[0].trim());
         holder.answerTV.setText(jokeParts[1].trim());
+        holder.parentCL.setBackgroundColor(getMatColor(TYPE_COLOR));
     }
 
     @Override
     public int getItemCount() {
         return listJokes.size();
+    }
+
+    /**
+     *
+     * @param typeColor takes the pallete ID
+     * @return a random color from a list of colors
+     */
+    private int getMatColor(String typeColor)
+    {
+        int returnColor = Color.BLACK;
+        int arrayId = context.getResources().getIdentifier("mdcolor_" + typeColor, "array", context.getApplicationContext().getPackageName());
+
+        if (arrayId != 0)
+        {
+            TypedArray colors = context.getResources().obtainTypedArray(arrayId);
+            int index = (int) (Math.random() * colors.length());
+            returnColor = colors.getColor(index, Color.BLACK);
+            colors.recycle();
+        }
+        return returnColor;
     }
 }
